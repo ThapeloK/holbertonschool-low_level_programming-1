68bda772698d84d0db640e9c612d3757a89364de
Farrukh <679@holbertonschool.com>
2019-02-12 17:40:47 +0000

    Added 10-add.c, which returns the sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* add - adds two numbers
+* @n: takes number
+* @m: second number
+*
+* Return: the sum of n and m
+*/
+
+int add(int n, int m)
+{
+	return (n + m);
+}
