c43d63c8464e80f43019f1f02fd039c3b1639fd8
Farrukh <679@holbertonschool.com>
2019-02-19 18:50:05 +0000

    Removed the aasigment of null character

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i]; i++)
+	for (i = 0; *(src + i); ++i)
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
 	}
-	dest[i] = src[i];
 	return (dest);
 }
