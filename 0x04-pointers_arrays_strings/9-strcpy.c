8326ed3a1a47dfe98fd5206556ec54742310230f
Farrukh <679@holbertonschool.com>
2019-02-19 16:19:47 +0000

    Using array instead of pointers

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; *(src + i); i++)
+	for (i = 0; src[i]; i++)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 	}
-	*(dest + i) = '\0';
+	dest[i] = src[i];
 	return (dest);
 }
