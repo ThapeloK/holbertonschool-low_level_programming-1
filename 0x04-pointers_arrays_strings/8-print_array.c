2c5559d64e2526a6a99b3778eeecd68a64852b64
Farrukh <679@holbertonschool.com>
2019-02-19 03:11:45 +0000

    Prints n elements of array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_array - prints array
+* @a: takes array
+* @n: takes int
+*
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%i\n", a[i]);
+		else
+			printf("%i, ", a[i]);
+	}
+}
