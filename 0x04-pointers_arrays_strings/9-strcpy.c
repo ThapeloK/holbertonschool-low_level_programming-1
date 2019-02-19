0e835815baeb4ec65563bda651363d1597c67e16
Farrukh <679@holbertonschool.com>
2019-02-19 05:52:48 +0000

    A clone to strcpy() function

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _strcpy - copies string
+* @dest: takes string
+* @src: takes string
+*
+* Return: void
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i); ++i)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
