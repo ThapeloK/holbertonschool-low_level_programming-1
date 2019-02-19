948c7a9b92805133aa01716540f7f6aecb50a46a
Farrukh <679@holbertonschool.com>
2019-02-19 19:25:36 +0000

    Added a condition

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,26 @@
 #include "holberton.h"
+
+#include "holberton.h"
+/**
+* _strlen - returns the length of the string
+* @s: takes string
+*
+* Return: returns the length int
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
 /**
 * puts2 - prints only odd numbered characters of a string
 * @str: takes string
@@ -8,10 +30,12 @@
 
 void puts2(char *str)
 {
-	while (*str)
+	int len, i;
+
+	len = 2 * (_strlen(str) / 2);
+	for (i = 0; i < len; i += 2)
 	{
-		_putchar(*str);
-		str += 2;
+		_putchar(*(str + i));
 	}
 	_putchar('\n');
 }
