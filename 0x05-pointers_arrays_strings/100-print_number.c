695dfe421329d1e425c899c25d5f5322a347a2ce
Farrukh <679@holbertonschool.com>
2019-02-21 22:41:28 +0000

    Prints integers using only _putchar. It is identical to the task from project 0x03, now it is compliant with betty

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
-* print_number - prints int with putchar
-* @n: takes number, hello 
-*
-* Return: void
-*/
+ * print_number - prints int with putchar
+ * @n: takes number
+ *
+ * Return: void
+ */
 
 void print_number(int n)
 {
 	unsigned int m;
-    
+
 	if (n < 0)
 	{
 		_putchar('-');
