e95ce816e43228a33c19aaa3a1b133fab0ba9d35
Farrukh <679@holbertonschool.com>
2019-02-25 21:14:01 +0000

    Prints the sum of diagonals of a matrix

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of diagonals of a matrix
+ * @a: multi dimensional array or matrix
+ *
+ * Return: Nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, d1 = 0, d2 = 0;
+
+	for (i = 0; i < size * size; i = i + 1 + size)
+	{
+		d1 += *(a + i);
+	}
+	for (i = size - 1; i < size * size - 1; i = i + size - 1)
+	{
+		d2 += *(a + i);
+	}
+	printf("%d, %d\n", d1, d2);
+}
