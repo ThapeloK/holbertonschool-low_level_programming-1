4fc753b971af74f7e4eb7e4d1e428ca5033525a7
Farrukh <679@holbertonschool.com>
2019-02-27 18:00:36 +0000

    Returns the length of a string

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - prints a length of a string
+ * @s: takes string
+ *
+ * Return: the length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s+1));
+	return (0);
+}
