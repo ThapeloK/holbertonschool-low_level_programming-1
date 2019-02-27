e3e1ef79b3cbf34359baabe91b7e0e09146f24c5
Farrukh <679@holbertonschool.com>
2019-02-27 18:08:41 +0000

    Calculates a factorial using recursion

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * factorial - factorial
+ * @n: takes int
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
