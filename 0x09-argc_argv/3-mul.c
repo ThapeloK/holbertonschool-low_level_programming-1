828cc2d79f27297875deebd76e2078a900005233
Farrukh <679@holbertonschool.com>
2019-03-02 20:34:04 +0000

    Changed condition, now checks if it has 3 arguments

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -12,12 +12,12 @@ int main(int argc, char **argv)
 {
 	int mul;
 
-	if (argc == 1)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
 	}
-	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", mul);
-	return (0);
+	printf("Error\n");
+	return (1);
 }
