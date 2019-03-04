76fd0e721be30748cde8997e415ad99e2fc0c2ea
Farrukh <679@holbertonschool.com>
2019-03-04 02:33:44 +0000

    This is another version, changed the order of declared variables

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -12,13 +12,15 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int i, j;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
+	int sum = 0;
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
