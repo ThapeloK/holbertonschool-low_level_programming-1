4d39cbe5854f4ee6bebec628de9ae3d916fe7ac5
Farrukh <679@holbertonschool.com>
2019-03-04 02:37:27 +0000

    This is another version, changed the order of declared variables and initializing sum after if statement

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -12,14 +12,14 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, j, sum;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	int sum = 0;
+	sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
