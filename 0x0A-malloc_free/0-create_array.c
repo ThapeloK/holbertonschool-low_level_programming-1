b2214a8156452b3915919dd83a059713461ad020
Farrukh <679@holbertonschool.com>
2019-03-05 20:38:23 +0000

    Added a terminating character

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -13,9 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	s = malloc(size);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
-		s[i] = c;
+				s[i] = c;
 	s[i] = '\0';
 	return (s);
 }
