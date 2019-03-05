79a822ba84fa28409b1e3bb6224e17061d318608
Farrukh <679@holbertonschool.com>
2019-03-05 17:41:00 +0000

    A clone of strdup() function

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strdup - duplicates a string
+ *
+ * Return: a pointer to the string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while(str[i])
+	{
+		i++;
+	}
+	s = malloc(sizeof(char) * i);
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while(str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i++] = '\0';
+	return (s);
+}
