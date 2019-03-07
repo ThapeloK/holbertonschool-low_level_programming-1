9e2a0d741b00430ef06b7ef6faf8d132d949846a
Farrukh <679@holbertonschool.com>
2019-03-07 20:38:18 +0000

    Added sizeof(char), and modified loop

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	s = malloc(sizeof(char) * nmemb * size);
 	if (!s)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 		s[i] = 0;
 	}
