ad8f3530633abf05880d5401abd263ba2b9f9d5d
Farrukh <679@holbertonschool.com>
2019-03-07 20:27:31 +0000

    Added sizeof(char)

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	s = malloc(nmemb * size);
+	s = malloc(sizeof(char) * nmemb * size);
 	if (!s)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
