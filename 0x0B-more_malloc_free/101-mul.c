f5a1b3e8b18f3665425ebc9db62ff754d99d39d9
Farrukh <679@holbertonschool.com>
2019-03-08 01:47:27 +0000

    Updated, handled an edge case

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -88,12 +88,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 int main(int argc, char **argv)
 {
-	int len1, len2, carry, a, b, i, j;
+	int len1, len2, carry, a, b, i, j, flag = 0;
 	int *result;
 
 	isError(argc, argv);
 	len1 = _strlen(argv[1]), len2 = _strlen(argv[2]);
-	/* printf("lena:%d lenb%d\n", len1, len2); */
+	if (argv[1][0] == 48 || argv[2][0] == 48)
+	{
+		printf("0\n");
+		return(0);
+	}
 	result = _calloc((len1 + len2), sizeof(char));
 	for (i = len1 - 1; i >= 0; i--)
 	{
@@ -102,18 +106,19 @@ int main(int argc, char **argv)
 		for (j = len2 - 1; j >= 0; j--)
 		{
 			b = argv[2][j] - '0';
-			/* printf("a: %d b: %d\n", a, b); */
-			carry += result[i + j + 1] + a * b;
+			carry += result[i + j + 1] + (a * b);
 			result[j + i + 1] = carry % 10;
 			carry /= 10;
 		}
-		if (carry)
+		if (carry > 0)
 			result[i + j + 1] += carry;
 	}
 	if (*result == 0)
 		result++;
 	for (a = 0; a < len1 + len2 - 1; a++)
+	{
 		printf("%d", result[a]);
+	}
 	printf("\n");
-		return (0);
+	return (0);
 }
