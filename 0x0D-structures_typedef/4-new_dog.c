6c204c2ec49c71a669aaa53837884aee83645f02
Farrukh <679@holbertonschool.com>
2019-03-11 19:25:09 +0000

    Function which creates a struct dog

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - prints struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *our_dog;
+
+	our_dog = malloc(sizeof(dog_t));
+	if (!our_dog)
+		return (NULL);
+	our_dog->name = name;
+	our_dog->age = age;
+	our_dog->owner = owner;
+	return (our_dog);
+}
