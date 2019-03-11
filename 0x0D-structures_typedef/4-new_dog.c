8f4e1587a969c1e7e9c77bf2430b78ef23e6cb58
Farrukh <679@holbertonschool.com>
2019-03-11 20:58:11 +0000

    Added condition for leak memory

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -12,18 +12,30 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *our_dog;
+	int i, j;
 
 	our_dog = malloc(sizeof(dog_t));
 	if (!our_dog)
 		return (NULL);
-	our_dog->name = malloc(sizeof(name));
+	for (i = 0; name[i]; i++)
+		;
+	for (j = 0; owner[j]; j++)
+		;
+	our_dog->name = malloc(sizeof(char) * i + 1);
 	if (!our_dog->name)
+	{
+		free (our_dog);
 		return (NULL);
+	}
 	our_dog->name = name;
 	our_dog->age = age;
-	our_dog->owner = malloc(sizeof(owner));
+	our_dog->owner = malloc(sizeof(char) * j + 1);
 	if (!our_dog->owner)
+	{
+		free (our_dog->name);
+		free (our_dog);
 		return (NULL);
+	}
 	our_dog->owner = owner;
 	return (our_dog);
 }
