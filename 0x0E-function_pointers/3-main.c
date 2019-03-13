d2cd1b12f2751affd3b65eb2c9917df7ea6e9935
Farrukh <679@holbertonschool.com>
2019-03-13 22:18:21 +0000

    main file

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -8,28 +8,31 @@
  * @argv: an array of arguments
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	char *op = argv[2];
-	int num2 = atoi(argv[3]);
+	int num1;
+	int num2;
+	int (*checker)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((*op == '/' || *op == '%') && num2 == 0)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(argv[2]) == NULL)
+	checker = get_op_func(argv[2]);
+	if (!checker)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	printf("%d\n", get_op_func(argv[2])(num1, num2));
 
-	return (0);
-}
+		 return (0);
+		 }
