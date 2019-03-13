f1d12451a8cb00869ce5d6e6f59e96010566ff79
Farrukh <679@holbertonschool.com>
2019-03-13 20:29:28 +0000

    5 functions to implement

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: first argument
+ * @b: second argument
+ * Return: the result of addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substraction
+ * @a: first argument
+ * @b: second argument
+ * Return: the result of a substraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: first argument
+ * @b: second argument
+ * Return: the result of multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a: first argument
+ * @b: second argument
+ * Return: the result of a division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus operator
+ * @a: first argument
+ * @b: second argument
+ * Return: the result of modulus operator
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
