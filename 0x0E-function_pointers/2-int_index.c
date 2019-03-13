6b732520123efee16d9fb12ac0cca1a47d657674
Farrukh <679@holbertonschool.com>
2019-03-13 20:23:06 +0000

    Compares the index of an array

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -2,13 +2,17 @@
 
 /**
  * int_index - searches for an integer inside of an array
- *
+ * @array: array
+ * @size: size of the array
+ * @cmp: function
  * Return: the index of found array
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (!array || cmp)
+		exit(1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
