814fc69d2e5722dbfaf76ec5d85448b528b12876
Farrukh <679@holbertonschool.com>
2019-03-14 17:39:57 +0000

    Refractored my code

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -5,20 +5,15 @@
  * @array: array
  * @size: size of the array
  * @cmp: function
- * Return: the index of found array
+ * Return: the index of found array element
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		if ((*cmp)(array[i]))
-			return (i);
-	}
+	if (array && cmp)
+		for (i = 0; i < size; i++)
+			if ((*cmp)(array[i]))
+				return (i);
 	return (-1);
 }
