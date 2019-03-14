cd0016d900ca068e1fa74bba9f39dc8e5402c9e5
Farrukh <679@holbertonschool.com>
2019-03-14 01:23:20 +0000

    Opcodes of a function

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: an array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	char *obj = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num1  = atoi(argv[1]);
+	if (num1 < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (num1--)
+	{
+		printf("%02hhx ", *obj++);
+	}
+	printf("\n");
+	return (0);
+}
