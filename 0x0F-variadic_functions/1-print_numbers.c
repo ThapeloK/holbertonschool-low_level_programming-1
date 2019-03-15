c01a69cf264775577eedab060002bf4aec980804
Farrukh <679@holbertonschool.com>
2019-03-15 16:50:17 +0000

    Prints all passed numbers

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: separator to be printed
+ * @n: number of arguments
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vp;
+	unsigned int i;
+
+	va_start(vp, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s", va_arg(vp, int),
+		       separator ? (i == n - 1 ? "\n" : separator) : "");
+	}
+	va_end(vp);
+}
