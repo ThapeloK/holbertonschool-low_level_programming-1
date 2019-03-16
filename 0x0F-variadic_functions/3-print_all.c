66a5ffab2ff94da6003d6ea9b0b7a2fe7f1db2ed
Farrukh <679@holbertonschool.com>
2019-03-16 19:48:36 +0000

    Mini printf function

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+/**
+ * print_all - mini printf function which uses printf
+ * @format: format specifier
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int a = 0;
+	char *copy, *sep = "";
+
+	if(!format)
+		return;
+	va_start(list, format);
+	while (format[a])
+	{
+		switch (format[a])
+		{
+		case 'c':
+			printf("%s%c", sep, va_arg(list, int));
+			break;
+		case 'i':
+			printf("%s%i", sep, va_arg(list, int));
+			break;
+		case 'f':
+			printf("%s%f", sep, va_arg(list, double));
+			break;
+		case 's':
+			copy = va_arg(list, char *);
+			if (!copy)
+				copy = "(nil)";
+			printf("%s%s", sep, copy);
+			break;
+		default:
+			a++;
+			continue;
+		}
+		sep = ", ", a++;
+	}
+	va_end(list);
+	putchar('\n');
+}
