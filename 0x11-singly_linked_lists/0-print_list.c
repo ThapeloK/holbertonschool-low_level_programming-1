bdd831a13a67ba3c7b99d52d47792463fb2da9e5
Farrukh <679@holbertonschool.com>
2019-03-21 16:52:43 +0000

    Added a header file and function for printing the cintents of a linked lists

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - simple function to print the lements of a linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == '\0')
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
