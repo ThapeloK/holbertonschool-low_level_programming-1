e904b597fb28de13ba82e40d97feb4b8b4ec7d2d
Farrukh <679@holbertonschool.com>
2019-03-21 19:37:43 +0000

    Frees linked list

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -7,16 +7,16 @@
  */
 void free_list(list_t *head)
 {
-	list_t *next_addr = head;
-	list_t *prev_addr = head;
+	list_t *current_addr = head;
+	list_t *next_addr;
 
-	while (next_addr->next != NULL)
+	while (current_addr->next != NULL)
 	{
-		free(next_addr->str);
-		next_addr = next_addr->next;
-		free(prev_addr);
-		prev_addr = next_addr;
+		free(current_addr->str);
+		next_addr = current_addr->next;
+		free(current_addr);
+		current_addr = next_addr;
 	}
-	free(next_addr->str);
-	free(prev_addr);
+	free(current_addr->str);
+	free(current_addr);
 }
