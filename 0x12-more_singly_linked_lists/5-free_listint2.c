63ba7c7a86d5f9915dc53942cc175d0932031885
Farrukh <679@holbertonschool.com>
2019-03-25 16:55:50 +0000

    Frees linked list

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to head
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+
+	while (*head)
+	{
+		prev = *head;
+		*head = (*head)->next;
+		free(prev);
+	}
+	*head = NULL;
+}
