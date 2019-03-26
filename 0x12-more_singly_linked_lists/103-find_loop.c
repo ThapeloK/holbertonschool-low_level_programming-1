ad916aa2d04b41af3ef036dccca9d48324bc0b1a
Farrukh <679@holbertonschool.com>
2019-03-26 22:11:46 +0000

    Finds a loop node in a linked list

--- 0x12-more_singly_linked_lists/103-find_loop.c
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -7,13 +7,24 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *temp = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	if (!head)
-		return (NULL);
-	while (temp && temp > temp->next)
-		temp = temp->next;
-	if (temp)
-		return (temp->next);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
 	return (NULL);
 }
