f9317492154da95fc6bbd62b4ef0b6d6bfb73cc0
Farrukh <679@holbertonschool.com>
2019-03-26 21:47:37 +0000

    Finds a loop inside linked list

---
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the first member of a linked list
+ * @head: pointer to the head
+ * Return: number of nodes
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *temp = (listint_t *)head;
+
+	while (temp && temp > temp->next)
+		temp = temp->next;
+	if (temp)
+		return (temp->next);
+
+	return (NULL);
+}
