a9d8f72c02f0a954566e5370c2c35dda40a0a7a0
Farrukh <679@holbertonschool.com>
2019-03-28 16:17:16 +0000

    Converts binary to unsigned int

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converst binary to uint
+ * @b: binary number
+ * Return: converted uint
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	uint num = 0, val = 1, len = 0;
+	int  i;
+
+	while (b[len])
+	{
+		if (!b[0] || (b[len] != '0' && b[len] != '1'))
+			return (0);
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			num += val;
+		val *= 2;
+	}
+	return (num);
+}
