709e98129b8ee7d5ddcc44cdc3de9134526dcba7
Farrukh <679@holbertonschool.com>
2019-03-28 21:12:04 +0000

    Added a proper condition

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets a bit in given position
+ * @n: number
+ * @index: index
+ * Return: number with setted position
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if ((*n > 0 || *n == 0) && (index > 0 || (index == 0)))
+	{
+		return (*n = *n | (1 << index));
+	}
+	return (-1);
+}
