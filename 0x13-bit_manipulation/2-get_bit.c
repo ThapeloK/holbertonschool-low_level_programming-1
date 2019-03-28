f9da47c573ea1c330165d05541f9ecc298e6bc42
Farrukh <679@holbertonschool.com>
2019-03-28 21:09:44 +0000

    Changed condition, for index and n

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -8,12 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int m = 0;
-
 	if ((n > 0 || n == 0) && (index > 0 || (index == 0)))
 	{
-		m = (n >> index & 1);
-		return (m);
+		return (n >> index & 1);
 	}
 	return (-1);
 }
