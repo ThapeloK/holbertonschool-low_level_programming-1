19bbcec50d3ab2beab80ffe86839aab574210e71
Farrukh <679@holbertonschool.com>
2019-04-02 18:01:01 +0000

    Added free statements inside conditions

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -25,6 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	i = read(fd, buf, letters);
 	if (i == -1)
 	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
@@ -33,6 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	j = write(STDOUT_FILENO, buf, i);
 	if (j == -1)
 	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
