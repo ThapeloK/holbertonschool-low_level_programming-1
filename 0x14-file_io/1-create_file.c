1d047adb10c66f9b5547bc98f8b7b9e95d45f89f
Farrukh <679@holbertonschool.com>
2019-04-02 16:46:05 +0000

    Creates a file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * create_file - creates a file
+ * @filename: filename to be created
+ * @text_content: content of a file
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, i = 0;
+	size_t len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		len = strlen(text_content);
+		i = write(fd, text_content, len);
+		if (i == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
