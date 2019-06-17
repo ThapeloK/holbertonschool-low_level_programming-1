8714448952352b7f3886b742b2841905a4582998
Farrukh <679@holbertonschool.com>
2019-06-17 19:03:14 +0000

    Quick sort

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -15,49 +15,51 @@ void swap(int *n1, int *n2)
 	*n2 = temp;
 }
 
+/**
+ * partition - Swaps two numbers
+ * @arr: Array
+ * @low: First number
+ * @high: Second number
+ * @size: Size of the @array
+ * Return: Nothing
+ */
+int partition(int *arr, int low, int high, size_t size)
+{
+	int pivot = arr[high];
+	int i = (low - 1), j;
 
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+			print_array(arr, size);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	print_array(arr, size);
+	return (i + 1);
+}
 
 /**
- * partition_lomuto_sort - quick sort
- * @array: Array
- * @low: low index
- * @high: high index
- * @size: Size of the array
+ * quickSort - Swaps two numbers
+ * @arr: Array
+ * @low: First number
+ * @high: Second number
+ * @size: Size of the @array
+ * Return: Nothing
  */
-void partition_lomuto_sort(int *array, int low, int high, int size)
+void quickSort(int *arr, int low, int high, size_t size)
 {
-	int current_low = low;
-	int current_high = high;
-	int pivot;
+	int pi;
 
-	if (low >= high)
-		return;
-
-	pivot = array[high];
-	while (current_low < current_high)
+	if (low < high)
 	{
-		while (current_low < current_high && array[current_low] < pivot)
-		{
-			current_low++;
-		}
-		while (current_low < current_high && array[current_high] >= pivot)
-		{
-			current_high--;
-		}
-		if (current_low < current_high)
-		{
-			swap(&array[current_low], &array[current_high]);
-			print_array(array, size);
-		}
+		pi = partition(arr, low, high, size);
+		quickSort(arr, low, pi - 1, size);
+		quickSort(arr, pi + 1, high, size);
 	}
-	if (current_high < high)
-	{
-		swap(&array[current_high], &array[high]);
-		print_array(array, size);
-	}
-
-	partition_lomuto_sort(array, low, current_high - 1, size);
-	partition_lomuto_sort(array, current_high + 1, high, size);
 }
 
 /**
@@ -70,5 +72,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
 		return;
-	partition_lomuto_sort(array, 0, size - 1, size);
+	quickSort(array, 0, size - 1, size);
 }
