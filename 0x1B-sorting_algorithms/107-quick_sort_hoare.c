e719e22027538d47f8d939483f13e9645ed322c1
Farrukh <679@holbertonschool.com>
2019-06-19 23:55:59 +0000

    Quick sort, Hoare

---
+++ 0x1B-sorting_algorithms/107-quick_sort_hoare.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two numbers
+ * @n1: First number
+ * @n2: Second number
+ * Return: Nothing
+ */
+void swap(int *n1, int *n2)
+{
+	int temp;
+
+	temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+/**
+ * partition - Swaps two numbers
+ * @arr: Array
+ * @low: First number
+ * @high: Second number
+ * @size: Size of the @array
+ * Return: Nothing
+ */
+int partition(int *arr, int low, int high, size_t size)
+{
+	int pivot = arr[high];
+	int i, j;
+
+	for (i = low, j = high; ; i++, j--)
+	{
+		while (arr[i] < pivot)
+			i++;
+		while (arr[j] > pivot)
+			j--;
+		if (i > j)
+			return j;
+		print_array(arr, size);
+		swap(&arr[i], &arr[j]);
+	}
+
+}
+
+/**
+ * quickSort - Swaps two numbers
+ * @arr: Array
+ * @low: First number
+ * @high: Second number
+ * @size: Size of the @array
+ * Return: Nothing
+ */
+void quickSort(int *arr, int low, int high, size_t size)
+{
+	int pi;
+
+	if (low < high)
+	{
+		pi = partition(arr, low, high, size);
+		quickSort(arr, low, pi, size);
+		quickSort(arr, pi + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Quick sorting algorithm
+ * @array: Array
+ * @size: Size of the @array
+ * Return: Nothing
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	quickSort(array, 0, size - 1, size);
+}
