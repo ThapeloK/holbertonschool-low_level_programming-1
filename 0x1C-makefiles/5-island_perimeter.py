e4edcc93ff9080e1bfa6e99a1da41be70063e0a4
Farrukh <679@holbertonschool.com>
2019-07-11 19:54:29 +0000

    Add checks for valid grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,6 +7,14 @@ Function which finds the perimeter of an island
 def island_perimeter(grid):
     """Function to find the perimeter of an island"""
     res = 0
+    if not grid:
+        return
+    if not all(type(arr) == list for arr in grid):
+        return
+    l = len(grid[0])
+    if not all(len(arr) == l for arr in grid):
+        return
+
     for i, arr in enumerate(grid):
         for j, val in enumerate(arr):
             if (val == 1):
