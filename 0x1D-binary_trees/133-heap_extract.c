e0c60e77c0fe9668f2b93b24540119292fc00a48
Farrukh <679@holbertonschool.com>
2019-07-26 21:37:56 +0000

    Add dependancies

--- 0x1D-binary_trees/133-heap_extract.c
+++ 0x1D-binary_trees/133-heap_extract.c
@@ -1,6 +1,95 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: binary tree
+ * Return: height of the @tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height;
+	size_t r_height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+	{
+		l_height = 1 + binary_tree_height(tree->left);
+	}
+	else
+	{
+		l_height = 0;
+	}
+
+	if (tree->right)
+		r_height = 1 + binary_tree_height(tree->right);
+	else
+	{
+		r_height = 0;
+	}
+
+	if (l_height > r_height)
+		return (l_height);
+	return (r_height);
+}
+
+/**
+ * find_depth - returns depth of leftmost leaf
+ * @tree: root tree or subtree
+ * Return: depth
+ */
+int find_depth(const binary_tree_t *tree)
+{
+	int depth = 0;
+
+	while (tree)
+	{
+		depth++;
+		tree = tree->left;
+	}
+	return (depth);
+}
+
+/**
+ * is_perfect_recursion - recursive function to check if bt is perfect
+ * @tree: root
+ * @depth: depth
+ * @level: checks level
+ * Return: 1 if true, 0 if false
+ */
+int is_perfect_recursion(const binary_tree_t *tree, int depth, int level)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (depth == level + 1);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	return (is_perfect_recursion(tree->left, depth, level + 1) &&
+		is_perfect_recursion(tree->right, depth, level + 1));
+}
+
+/**
+ * binary_tree_is_perfect - function that checks if a binary tree is perfect
+ * @tree: binary tree
+ * Return: 1 if @tree is perfect
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int depth;
+
+	if (tree == NULL)
+		return (0);
+
+	depth = find_depth(tree);
+
+	return (is_perfect_recursion(tree, depth, 0));
+}
+
+/**
  * swap - Swaps two numbers
  * @n1: First number
  * @n2: Second number
