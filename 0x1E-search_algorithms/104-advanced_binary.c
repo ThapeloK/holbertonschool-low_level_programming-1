1e9374eb588bdb9d7ca0d60c37cde9dc453b4a91
Farrukh <679@holbertonschool.com>
2019-08-28 22:05:15 +0000

    Add advanced binary

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -37,12 +37,11 @@ int binary_search_rec(int *array, size_t start, size_t end,  int value)
 
 	if (start > end)
 		return (-1);
+
 	mid = start + (end - start) / 2;
 	printer(array, start, end);
-	if (array[mid] == value)
+	if (array[mid] == value && array[mid - 1] != value)
 	{
-		if (array[mid] == array[mid - 1])
-			return (binary_search_rec(array, start, mid, value));
 		return (mid);
 	}
 	if (array[mid] < value)
